2017d44f2324a72308ab51b96799ffe53f9c5de6
song-ouyang <1723767677@qq.com>
2021-11-03 10:48:24 +0800

    oys第三次

--- app/Http/Controllers/Login/MailController.php
+++ app/Http/Controllers/Login/MailController.php
@@ -7,18 +7,31 @@ use Illuminate\Support\Facades\Mail;
 
 class MailController extends Controller
 {
-    public function send()
+    public function send(Request $request)
     {
-        Mail::raw("这是测试的内容", function ($message){
+
+        $yzm = "";
+        for($i=0;$i<4;$i++)
+        {
+            $a = rand(0,9); //0-9随机数
+            $yzm.= $a;
+        }
+        
+        $email=$request->get('email');
+      
+        Mail::raw($yzm, function ($message)use ($email){
             // * 如果你已经设置过, mail.php中的from参数项,可以不用使用这个方法,直接发送
             // $message->from("1182468610@qq.com", "laravel学习测试");
-            $user='1114621937@qq.com';
+            $user=$email;
             $message->subject("测试的邮件主题");
             // 指定发送到哪个邮箱账号
             $message->to($user);
         });
 
-        // 返回的一个错误数组，利用此可以判断是否发送成功
-        dd(Mail::failures());
+        return $yzm ?
+            json_success('获取邮箱成功!', $yzm, 200) :
+            json_fail('获取邮箱失败!', null, 100);
+
+
     }
 }
--- config/mail.php
+++ config/mail.php
@@ -56,7 +56,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'MAIL_FROM_ADDRESS'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
